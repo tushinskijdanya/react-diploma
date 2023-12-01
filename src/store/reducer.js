1a555bafd4054fb36b2a96de315c2a8b115fbb0e
Daniil Tushinsky <tushinskij.danya@gmail.com>
2023-12-01 15:46:23 +0300

    edit store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,13 +1,15 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { combineReducers, createStore } from "redux";
 
 import { goodsReducer } from "./goodsReducer";
 import { langCurrReducer } from "./langCurrReducer";
 
+
 const rootReducer = combineReducers({
-    goods: goodsReducer,
-    langCurr: langCurrReducer
+    // goods: goodsReducer,
+    // langCurr: langCurrReducer
+    gds: goodsReducer,
+    lng: langCurrReducer
 })
 
-export const shopStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const shopStore = createStore(rootReducer, composeWithDevTools());
